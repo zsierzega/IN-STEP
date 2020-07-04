101a0bd26b68b02973473fa41a1f982a23668b25
zsierzega <zsierzega@gmail.com>
2020-07-04 13:40:56 -0500

    update sankey

--- core/script.js
+++ core/script.js
@@ -6,50 +6,61 @@ data.addColumn('string', 'From');
 data.addColumn('string', 'To');
 data.addColumn('number', 'Weight');
 data.addRows([ 
+	['3-5 Life Science','LS 2: Ecosystems: Interactions, Energy, and Dynamics',1], 
+	['LS 2: Ecosystems: Interactions, Energy, and Dynamics','Human Climate',1], 
+	['3-5 Life Science','LS 4:  Biodiversity and Humans',1], 
+	['LS 4:  Biodiversity and Humans','Human Climate',1], 
 	['Science','3-5 Life Science',1], 
-	['3-5 Life Science','Drugs',1], 
-	['Drugs','Dynamics',1], 
-	['3-5 Earth & Space Science','Climate',1], 
-	['Climate','Elements',1], 
-	['Science','3-5 Earth & Space Science',1], 
-	['3-5 Phyical Science','Demolition',1], 
-	['Demolition','Pollinators',1], 
+	['3-5 Earth & Space Science','ESS 1: Earth\'s Place in the Universe',1], 
+	['ESS 1: Earth\'s Place in the Universe','Human Climate',1], 
+	['3-5 Earth & Space Science','ESS 2:Earth\'s Systems',1], 
+	['ESS 2:Earth\'s Systems','Human Climate',1], 
+	['3-5 Earth & Space Science','ESS 3:Earth and Human Activity',1], 
+	['ESS 3:Earth and Human Activity','Human Climate',1], 
+	['3-5 Engineering Design','ETS 1: Engineering Design',1], 
+	['ETS 1: Engineering Design','Human Climate',1], 
+	['3-5 Phyical Science','PS 3: Energy',1], 
+	['PS 3: Energy','Human Climate',1], 
 	['Science','3-5 Phyical Science',1], 
-	['6-8 Life Science','Earthquake',1], 
-	['Earthquake','Wrecking Ball',1], 
+	['6-8 Physical Science','PS 1: Matter and Its Interactions',1], 
+	['PS 1: Matter and Its Interactions','Illicit Drugs',1], 
+	['6-8 Physical Science','PS 2: Motion and Stability: Forces and Interactions',1], 
+	['PS 2: Motion and Stability: Forces and Interactions','Illicit Drugs',1], 
+	['6-8 Life Science','LS1: Information Processiong ',1], 
+	['LS1: Information Processiong ','Illicit Drugs',1], 
 	['Science','6-8 Life Science',1], 
-	['6-8 Earth & Space Science','Drugs',1], 
-	['Drugs','Safety',1], 
+	['6-8 Engineering Design','ETS1 Engineering Design',1], 
+	['ETS1 Engineering Design','Illicit Drugs',1], 
+	['6-8 Engineering Design','ETS 1: Engineering Design',1], 
+	['ETS 1: Engineering Design','Earthquake',1], 
+	['Engineering','6-8 Engineering Design',1], 
+	['6-8 Earth & Space Science','ESS 3: Earth and Human Activity',1], 
+	['ESS 3: Earth and Human Activity','Earthquake',1], 
 	['Science','6-8 Earth & Space Science',1], 
-	['6-8 Physical Science','Climate',1], 
-	['Climate','Dynamics',1], 
+	['6-8 Physical Science','PS 2: Motion and Stability: Forces and Interactions',1], 
+	['PS 2: Motion and Stability: Forces and Interactions','Earthquake',1], 
+	['6-8 Physical Science','PS 3: Energy',1], 
+	['PS 3: Energy','Earthquake',1], 
+	['6-8 Physical Science','PS 1: Matter and Its Interactions',1], 
+	['PS 1: Matter and Its Interactions','Earthquake',1], 
 	['Science','6-8 Physical Science',1], 
-	['K-2 Life Science','Demolition',1], 
-	['Demolition','Elements',1], 
-	['Science','K-2 Life Science',1], 
-	['K-2 Earth & Space Science','Earthquake',1], 
-	['Earthquake','Pollinators',1], 
-	['Science','K-2 Earth & Space Science',1], 
-	['K-2 Phyical Science','Drugs',1], 
-	['Drugs','Wrecking Ball',1], 
-	['Science','K-2 Phyical Science',1], 
-	['K-2 Engineering Design','Climate',1], 
-	['Climate','Safety',1], 
-	['Engineering','K-2 Engineering Design',1], 
-	['3-5 Engineering Design','Demolition',1], 
-	['Demolition','Dynamics',1], 
-	['Engineering','3-5 Engineering Design',1], 
-	['6-8 Engineering Design','Earthquake',1], 
-	['Earthquake','Safety',1], 
-	['Engineering','6-8 Engineering Design',1] 
-]);
-
-var options = {
-
-  width: 900,
-  height: 500,
-
+	['3-5 Phyical Science','PS 1: Matter and Its Interactions',1], 
+	['PS 1: Matter and Its Interactions','Population Density',1], 
+	['3-5 Life Science','LS 1: From Molecules to Organisms: Structures and Processes',1], 
+	['LS 1: From Molecules to Organisms: Structures and Processes','Population Density',1], 
+	['3-5 Earth & Space Science','ESS 3: Earth and Human Activity',1], 
+	['ESS 3: Earth and Human Activity','Population Density',1], 
+	['Science','3-5 Earth & Space Science',1], 
+	['3-5 Engineering Design','ETS 1: Engineering Design',1], 
+	['ETS 1: Engineering Design','Population Density',1], 
+	['Engineering','3-5 Engineering Design',1] 
+]); 
+var options = { 
+ 	width: 900,
+	height: 500,
 }
+
 var chart = new google.visualization.Sankey(document.getElementById('sankey_basic'));
-        chart.draw(data, options);
-} 
+
+chart.draw(data, options);
+}
